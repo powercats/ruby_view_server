15a14d3c878e24e9e707f8e7022fc3cdf4aec5c6
Harry LEE <sendittohenry@gmail.com>
2013-10-10 23:37:39 -0700

    updated accoridng to homework section

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,12 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+hash = {cat: "meow", dog: "poop"}
+template = ERB.new "A cat goes <%= hash[:cat] %>"
+puts template.result(binding)
+
+stevie = "hates bread"
+template = ERB.new "Stevie hates <%= stevie %>"
+puts template.result(binding)
+
